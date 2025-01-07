a41349738f1d9fb85dc0e1b43ff3e11726df50a8
Ahmadjon <@robotoprotecno2008@gmail.com>
2025-01-07 12:54:02 +0500

    Work with errors / Xatolarni tutish

---
+++ 16_learning.py
@@ -0,0 +1,38 @@
+# Python da xatoliklarni tutish: Try; Exept;
+
+"""
+Agar xato nomi bilinmasa dasturni o'zini yurg'azilganidan chiqadigan xato nomi bo'ladi!
+Nomi bilinmasa 'exeptni' o'zini yozishimiz mumkin!
+"""
+
+# try:
+#     print(sal)
+# except NameError:
+#     print("Xato dastur")
+
+# try:
+#     a = i
+#     for i in range(a):
+#         a += i
+#     print(a)
+# except:
+#     print("Bu dastur xato")
+
+
+## With else & finally:
+
+# try:  # Entered
+#     print("Hello world!")
+# except:  # Not entered;
+#     print("This is wrong!")
+# else:  # Entered
+#     print("Nothing wrong!")
+# finally:  # Entered
+#     print("This project is ended!")
+
+# Work with Error
+
+# i = "txt"
+#
+# if not type(i) is int:
+#     raise TypeError("Only integers can enter to this variable!")
